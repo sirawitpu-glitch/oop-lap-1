4f81705c7f0725860efd9897166c43fbcdaa95b2
sirawitpu-glitch <sirawit.pu@kkumail.com>
2025-12-02 10:44:05 +0700

    add c#

---
+++ Hello word ConsoleApp1/Hello word ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_word_ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Sirawit Putongjai");
+            Console.WriteLine("Student ID: 683450063-2");
+            Console.WriteLine("Major: Computer and Information Science");
+            Console.WriteLine("Faculty: Is");
+            Console.WriteLine("khon Kaen University");
+        }
+    }
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# oop-lap-1
-my name is sirawit
-my student id:683450063-2
